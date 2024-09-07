ff15f4b36a99b4848fa0f799fc3931a684357ee9
Dylan Schilder <dylan.schilder3@hotmail.com>
2024-09-07 13:59:56 +0200

    Enemy and HQ functionality

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform HQLocation;
+    
+    public int enemyHP;
+    public int enemyDMG;
+    public int enemyGoldValue;
+    void Start()
+    {
+        enemyHP = 50;
+        enemyDMG = 5;
+        enemyGoldValue = 25;
+        agent.SetDestination(HQLocation.position);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "HQ")
+        {
+            HQManager.hqHP = HQManager.hqHP - enemyDMG;
+            Debug.Log("" +HQManager.hqHP);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HQManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HQManager : MonoBehaviour
+{
+    public static int hqHP;
+    public static int currentGold;
+    void Start()
+    {
+        hqHP = 100;
+        currentGold = 500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
