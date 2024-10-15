0b6141dede0a4e14b229defec7e7029bbd77b771
Dylan Schilder <dylan.schilder3@hotmail.com>
2024-10-15 17:33:37 +0200

    removed comment

--- Assets/Scripts/TurretRotation.cs
+++ Assets/Scripts/TurretRotation.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class TurretRotation : MonoBehaviour
 {
-    /*public Transform target;
-
-    //Update is called once per frame:
-    void FixedUpdate()
-    {
-        //Find enemy and look at enemy:
-        target = GameObject.FindGameObjectWithTag("Enemy1").transform;
-        transform.LookAt(target);
-    }*/
+    
 }
